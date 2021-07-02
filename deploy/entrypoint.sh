11f1ced573f9372cb4859180c905f4776798bac1
almed4 <alexandre.meddin@ingka.ikea.com>
2021-07-02 11:33:29 -0400

    add deploy

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -9,7 +9,6 @@ printStepExecutionDelimiter() {
   printf "\n----------------------------------------\n"
 }
 
-
 validateProperty() {
   PROP_KEY=$1
   PROP_VALUE=$(echo "$SECRETS" | grep "$PROP_KEY" | cut -d'=' -f2)
@@ -17,16 +16,17 @@ validateProperty() {
     echo "Error validating SECRETS: $PROP_VALUE empty."
     return 1
   else
+    eval export "$PROP_KEY"="$PROP_VALUE"
     return 0
   fi
 }
 
 validateEnvironment() {
+  printf "\n\nValidating Secrets."
   printLargeDelimiter
-  echo "Validating Secrets"
-  printDelimiter
-  echo "Validating GITHUB_USER"
-  if ! validateProperty "GITHUB_USER"; then
+
+  echo "Validating GITHUB_ACTOR"
+  if ! validateProperty "GITHUB_ACTOR"; then
     return 1
   fi
   echo "Validating GITHUB_TOKEN"
@@ -41,31 +41,96 @@ validateEnvironment() {
   if ! validateProperty "IKEA_ARTIFACTORY_PASSWORD"; then
     return 1
   fi
-  printStepExecutionDelimiter
+
   echo "Secrets validated!"
   return 0
 }
 
 populateEnvironment() {
+  printf "\n\nPopulating environment."
   printLargeDelimiter
-  echo "Populating environment."
-  VERSION=$([ "${GITHUB_EVENT_NAME}" == "release" ] && echo "${GITHUB_REF##*/}" || echo "latest")
-  ACTION=$([[ ( "${GITHUB_EVENT_NAME}" == 'push' && "${GITHUB_REF}" == 'refs/heads/develop' ) || "${GITHUB_EVENT_NAME}" == 'release' ]] && echo "--push" || echo "--load")
-  IMAGE="artifactory.build.ingka.ikea.com/ushub-docker-dev-local/${GITHUB_REPOSITORY##*/}:$VERSION"
-  PATH="https://${{ github.actor }}:${{ github.token }}@github.com/${GITHUB_REPOSITORY}.git#${GITHUB_REF#*/}"
-  HELMFILE="https://${{ github.actor }}:${{ github.token }}@raw.githubusercontent.com/${GITHUB_REPOSITORY}/${GITHUB_REF##*/}/helmfile.yaml"
-  ENVIRONMENT=$(case ${GITHUB_EVENT_ACTION} in
-    released)
+
+  VERSION=$([ "$GITHUB_EVENT_NAME" = "release" ] && echo "${GITHUB_REF##*/}" || echo "latest")
+  echo "VERSION=$VERSION"
+  export IMAGE="artifactory.build.ingka.ikea.com/ushub-docker-dev-local/${GITHUB_REPOSITORY##*/}:$VERSION"
+  echo "IMAGE=$IMAGE"
+  HELMFILE="https://$GITHUB_ACTOR:$GITHUB_TOKEN@raw.githubusercontent.com/$GITHUB_REPOSITORY/develop/helmfile.yaml"
+  #  HELMFILE="https://$GITHUB_ACTOR:$GITHUB_TOKEN@raw.githubusercontent.com/$GITHUB_REPOSITORY/${GITHUB_REF##*/}/helmfile.yaml"
+  echo "HELMFILE=***@$(echo "HELMFILE=$HELMFILE" | sed "s/.*@//")"
+  ENVIRONMENT=$(case "$GITHUB_EVENT_ACTION" in
+    "released")
       echo 'prod'
-    ;;
-    prereleased)
+      ;;
+    "prereleased")
       echo 'stage'
-    ;;
+      ;;
     *)
       echo 'dev'
-    ;;
-  esac)
+      ;;
+    esac)
+  echo "ENVIRONMENT=$ENVIRONMENT"
+
+  echo "Environment populated!"
+  return 0
+}
+
+prepHelmEnvironment() {
+  printf "\n\nPreparing Helm environment."
+  printLargeDelimiter
+
+  mkdir ~/.kube
+  echo "$KUBECONFIG" >>~/.kube/config
+  chmod 400 ~/.kube/config
+  echo "KUBECONFIG created."
+
+  HELM_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "$HELMFILE") # | sed ':a;N;$!ba;s/\n//g')
+  if [ "$HELM_RESPONSE" = "200" ]; then
+    curl -s "$HELMFILE" >>helmfile.yaml
+  else
+    echo "Error retrieving Helmfile: $HELM_RESPONSE"
+    return 1
+  fi
+
+  echo "Helm environment prepared!"
+  return 0
+}
+
+# This step must be completed because Helmfile doesn't have support for
+# --pass-credentials, which is necessary to pull from our repo (and Helm-Git
+# doesn't work with versioned charts)
+#
+# Hopefully fixed by: https://github.com/roboll/helmfile/issues/1898
+addHelmRepo() {
+  printf "\n\nAdding Helm repository."
+  printLargeDelimiter
+
+  helm repo add \
+    --username "$GITHUB_ACTOR" \
+    --password "$GITHUB_TOKEN" \
+    --pass-credentials \
+    usdh-common-helm \
+    https://raw.githubusercontent.com/ingka-group-digital/usdh-common-helm/prod/repo
+
+  echo "Helm repo added!"
+  return 0
+}
+
+syncHelmfile() {
+  printf "\n\nSyncing Helmfile."
+  printLargeDelimiter
+
+  helmfile -e "$ENVIRONMENT" sync
+
+  echo "Helmfile synced!"
+  return 0
 }
 
 validateEnvironment
 populateEnvironment
+prepHelmEnvironment
+addHelmRepo
+syncHelmfile
+
+printLargeDelimiter
+printf "\n\nApplication Deployed!.\n\n"
+printLargeDelimiter
